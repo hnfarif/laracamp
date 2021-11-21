04e5ee45fe2aae56f05ddff5f383fd9222e3b4f0
Hanif Arif <hnfarif18@gmail.com>
2021-11-21 15:27:46 +0700

    slicing frontend done

--- bwa-laracamp/routes/web.php
+++ bwa-laracamp/routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+Route::get('/checkout', function () {
+    return view('checkout');
+})->name('checkout');
+Route::get('/checkout-success', function () {
+    return view('success-checkout');
+})->name('success-chk');
